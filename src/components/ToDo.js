f1ab423ab0058e0af0836b4dd50b0e6dba610baf
Hitesh Patel <hbp976@gmail.com>
2020-09-11 13:35:04 -0600

    added uuid for todo list

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
-function ToDo ()
-{
-  //  const [num1, num2] = [1,2];
-   // console.log( num1 );
+function ToDo() {
+    //  const [num1, num2] = [1,2];
+    // console.log( num1 );
     //console.log( num2 );
 
-    const [newTask, setNewTask] = useState( 'default' );
+    const [newTask, setNewTask] = useState('default');
 
-    const [toDos, setToDos] = useState( [ 
-        { task: "Buy milk" },
-        { task: "Learn Reacy" },
-        { task: "Find out what Redux is" }
-    ] );
+    const [toDos, setToDos] = useState([
+        { id: uuidv4(), task: "Buy milk" },
+        { id: uuidv4(), task: "Learn Reacy" },
+        { id: uuidv4(), task: "Find out what Redux is" }
+    ]);
 
     const addNewTask = event => {
         event.preventDefault();
 
         const newToDosList = [...toDos];
-        newToDosList.push( { task: newTask } );
+        newToDosList.push({ id: uuidv4(), task: newTask });
 
-        setToDos( newToDosList );
+        setToDos(newToDosList);
 
-        setNewTask( '' );
+        setNewTask('');
 
     }
 
@@ -32,14 +32,14 @@ function ToDo ()
         <>
             <form onSubmit={addNewTask}>
                 <label htmlFor="task">New Task:</label>
-                <input type="text" id="task" onChange={e => { setNewTask( e.target.value ) } } value={newTask} />
+                <input type="text" id="task" onChange={e => { setNewTask(e.target.value) }} value={newTask} />
                 <p>
                     <strong>Current Task Value: </strong>
                     <em>{newTask}</em>
                 </p>
                 <input type="submit" value="Add To-Do" />
             </form>
-            <ul>{toDos.map( ( toDo, index ) => <li key={index}>{toDo.task}</li> )}</ul>
+            <ul>{toDos.map( toDo => <li key={toDo.id}>{toDo.task}</li>)}</ul>
         </>
     );
 
